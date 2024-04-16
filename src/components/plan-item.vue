14fb7f3801d6f979bffbdcece35541c897a0259a
LANH27 <aleis720221.siul@gmail.com>
2024-04-16 11:30:12 -0600

    ⭐Feat:Properties

---
+++ src/components/plan-item.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="plan">
+        <div class="description">
+          <span class="title">
+            {{ name }}
+          </span>
+        </div>
+      </div>
+    </template>
+
+<script setup>
+const props = defineProps(['name']);
+console.log(props.name);
+</script>
+
