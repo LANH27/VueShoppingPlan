7d267ca0df2f4a99a11d906d8439c47482f4cfc2
LANH27 <aleis720221.siul@gmail.com>
2024-04-19 22:18:54 -0600

    ✨Nested Components

--- src/components/plan-picker-item.vue
+++ src/components/plan-picker-item.vue
@@ -10,22 +10,6 @@
 
 <script setup>
 
-//Componentes plan-picker;
-import vue from 'vue';
-
-function planPicker(props) {
-return (
-  <div>
-  <h3>Planes Disponibles </h3>
-  <ul>
-     {props.planes.map((plan, index )=>(
-      <li key={index}>{plan}</li>
-     ))}
-     </ul>
-    </div>
-);
-  }
-
 defineProps ({
     name: {
     type: String,
