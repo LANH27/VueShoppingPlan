5444466b83de457aa32c19b92248259cfc7f48c7
LANH27 <aleis720221.siul@gmail.com>
2024-04-26 11:59:04 -0600

    ☕Coffe plands done

--- src/components/plan-picker-item.vue
+++ src/components/plan-picker-item.vue
@@ -5,13 +5,15 @@
     class="plan">
         <div class="description">
           <span class="title">
-            {{ name }} ultimo seleccionado: {{ selectedPlan}}
+            {{ name }} 
           </span>
         </div>
       </div>
     </template>
 
 <script setup>
+import { computed } from 'vue';
+
 
 
 //Creando un evento
@@ -32,7 +34,10 @@ const select = () => {
 //Detomando el evento
   emit('select', props.name);
 }
-
+// creando propiedad computada
+const selected = computed (() => {
+  return prps.name === prps.selectPlan;
+});
 </script>
 <style scoped>
 .selected-plan {
